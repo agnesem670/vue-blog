3783994d04ec185a8c06515dc2196aa72683ea70
agnesem670 <agnesem670@gmail.com>
2023-11-06 23:08:41 +0100

    HomeView, DetailsView and basic logic

---
+++ src/components/PostList.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="post-list">
+    <div v-for="post in posts" :key="id">
+    <SinglePost :post="post"/>
+    </div>
+</div>
+</template>
+
+<script>
+import SinglePost from './SinglePost.vue';
+import { onMounted, onUnmounted, onUpdated } from 'vue'
+
+export default {
+    props: ['posts'],
+    components: {SinglePost},
+    setup(props) {
+        onMounted(() => {
+            console.log('componen mounted')
+        })
+        onUnmounted(() => {
+            console.log ('component unmounted')
+        })
+
+        onUpdated (() => {
+            console.log ('component updated')
+        })
+    },
+    components: { SinglePost }
+}
+</script>
+
+<style>
+</style>
